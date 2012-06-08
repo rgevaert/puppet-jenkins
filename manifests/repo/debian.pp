ee7b48f78dac4db05e1b9d08d1334de6e51969b3
Rudy Gevaert <Rudy.Gevaert@UGent.be>
2012-06-08 14:45:52 +0200

    Use the apt module at ugent

--- manifests/repo/debian.pp
+++ manifests/repo/debian.pp
@@ -1,10 +1,14 @@
 class jenkins::repo::debian {
-  apt::source { 'jenkins':
-    location    => 'http://pkg.jenkins-ci.org/debian',
-    release     => '',
-    repos       => 'binary/',
-    key         => 'D50582E6',
-    key_source  => 'http://pkg.jenkins-ci.org/debian/jenkins-ci.org.key',
-    include_src => false,
+
+  apt::sources_list {
+    'jenkins':
+        ensure  => present,
+        content => 'deb http://pkg.jenkins-ci.org/debian binary/';
+  }
+
+  apt::key {
+    'D50582E6':
+      ensure => present,
+      source => 'http://pkg.jenkins-ci.org/debian/jenkins-ci.org.key';
   }
 }
